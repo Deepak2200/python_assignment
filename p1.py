0f9c7aabb7f16b070dcf797e6e04f4efab50fb6f
Deepak2200 <119037072+Deepak2200@users.noreply.github.com>
2023-02-18 18:49:41 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,7 +0,0 @@
-mydict={'jan':47,'feb':52,'march':47,'april':44,'may':52,'june':53,'july':54,'aug':44,'sep':54}
-print("dictionary valuses ",mydict.values())
-newlist=list()
-for i in mydict.values():
-    if i not in newlist:
-        newlist.append(i)
-        print("unique list",newlist)
